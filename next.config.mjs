db1aa8554e2ee7b084252860adb325b056eb735c
nahidul31 <nahidulislam2383@email.com>
2026-04-14 20:43:07 +0600

    create out file for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
-  // picture: "https://randomuser.me/api/portraits/men/11.jpg",
+
+  output: "export",
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "randomuser.me",
-        port: "",
         pathname: "/api/**",
-        search: "",
       },
     ],
   },
