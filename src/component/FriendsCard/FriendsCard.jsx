b0349b4e0f6d8630cb3d624f7a3ef0e9194103d6
nahidul31 <nahidulislam2383@email.com>
2026-04-15 13:00:02 +0600

    show tags dynamically and make responsive other parts

--- src/component/FriendsCard/FriendsCard.jsx
+++ src/component/FriendsCard/FriendsCard.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const FriendsCard = ({ friend }) => {
   //   console.log(friend);
-  const { id, name, picture, days_since_contact, status } = friend;
+  const { id, name, picture, days_since_contact, status, tags } = friend;
   //   const handleShowbtn = (id) => {};
   return (
     <Link href={`friendDetails/${id}`}>
@@ -24,12 +24,23 @@ const FriendsCard = ({ friend }) => {
 
           {/* tags */}
           <div className="flex gap-2 mt-2">
-            <div className="badge badge-success p-3 text-white">WORK</div>
+            {tags.map((tag, idx) => (
+              <div
+                key={idx}
+                className="badge badge-success p-3 text-white capitalize"
+              >
+                {tag}
+              </div>
+            ))}
           </div>
 
           {/* status */}
           <div className="mt-3">
-            <div className="badge badge-warning p-3 text-white">Almost Due</div>
+            <div
+              className={`badge ${status === "almost due" ? "badge-warning" : status === "on-track" ? "bg-[#1a3d30]" : "badge-error"} p-3 text-white`}
+            >
+              {status}
+            </div>
           </div>
         </div>
       </div>
